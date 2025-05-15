923f68b493f6f45ad530b8b3c5fa53c598658a4c
Ad-cmd-1976 <adityavishwakarma9025@gmail.com>
2025-05-15 13:02:52 +0530

    fixed okay

--- backend/src/server.js
+++ backend/src/server.js
@@ -82,8 +82,8 @@ if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static(distPath));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(distPath, 'index.html'));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'));
   });
 }
 
